ba058cc66826c6b0e0984679e477b45ad1b02225
Jason Lam <jasonlam0619@duck.com>
2025-09-01 15:20:01 +0800

    Tweak library UI and add icons

--- src/views/Flashcards.vue
+++ src/views/Flashcards.vue
@@ -110,10 +110,44 @@ const current = computed(() => order.value[index.value])
   <div class="h-screen flex flex-col items-center justify-center p-4 relative">
     <div
       v-if="showSentence && current"
-      class="absolute top-4 right-4 flex space-x-2 z-20"
+      class="absolute top-4 right-4 flex flex-col space-y-2 z-20"
     >
-      <button class="text-blue-500" @click.stop="startEdit">Edit</button>
-      <button class="text-red-500" @click.stop="removeCurrent">Delete</button>
+      <button class="text-blue-500" @click.stop="startEdit" aria-label="Edit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+          />
+        </svg>
+      </button>
+      <button
+        class="text-red-500"
+        @click.stop="removeCurrent"
+        aria-label="Delete"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+          />
+        </svg>
+      </button>
     </div>
     <div ref="cardRef" class="card-wrapper w-full max-w-md h-64 relative">
       <div
@@ -145,10 +179,46 @@ const current = computed(() => order.value[index.value])
       </div>
     </div>
     <div class="flex gap-4 mt-4">
-      <button class="btn" @click="() => handleSwipe('right')">Prev</button>
-      <button class="btn" @click="() => handleSwipe('left')">Next</button>
+      <button class="btn inline-flex items-center gap-1" @click="() => handleSwipe('right')">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        </svg>
+        Prev
+      </button>
+      <button class="btn inline-flex items-center gap-1" @click="() => handleSwipe('left')">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-5 h-5"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
+        Next
+      </button>
     </div>
-    <router-link to="/library" class="mt-4 text-blue-500">Back to Library</router-link>
+    <router-link to="/library" class="mt-4 text-blue-500 inline-flex items-center gap-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-5 h-5"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+      </svg>
+      Back to Library
+    </router-link>
     <div
       v-if="editing"
       class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-30"
@@ -157,8 +227,32 @@ const current = computed(() => order.value[index.value])
         <textarea v-model="editForm.text" class="input" placeholder="Sentence" />
         <input v-model="editForm.translation" class="input" placeholder="Translation" />
         <div class="flex justify-end space-x-2">
-          <button type="button" class="btn-secondary" @click="cancelEdit">Cancel</button>
-          <button type="submit" class="btn">Save</button>
+          <button type="button" class="btn-secondary inline-flex items-center gap-1" @click="cancelEdit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+            Cancel
+          </button>
+          <button type="submit" class="btn inline-flex items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+            </svg>
+            Save
+          </button>
         </div>
       </form>
     </div>
