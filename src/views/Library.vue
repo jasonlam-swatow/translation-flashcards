9b7b69efba883b3f45e4d371c83d7b8791cceac9
林佳俊 <jiajun@datastory.com.cn>
2025-10-15 14:45:09 +0800

    fix: library note style

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -348,7 +348,7 @@ function reset() {
                 d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.02M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
               />
             </svg>
-            <span text-sm text-gray-700>{{ preview.note }}</span>
+            <span class="text-sm text-gray-700">{{ preview.note }}</span>
           </div>
         </div>
       </div>
